ea64420e006ec1c5796b62a125761a9a934b5a58
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-03-03 19:35:04 +0300

    сложил все в одну папку

---
+++ task34.cs
@@ -0,0 +1,40 @@
+﻿// // Задача 34: Задайте массив заполненный случайными положительными
+//  трёхзначными числами. Напишите программу, которая покажет
+//   количество чётных чисел в массиве.
+// // [345, 897, 568, 234] -> 2
+
+
+System.Console.WriteLine("Введите размер массива");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+
+int[]CreateArray (int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i<lenght; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    System.Console.WriteLine("["+ string.Join(", ", array)+"]");
+}
+
+int FillNumber(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i<lenght; i++) 
+    {
+        if (array[i]%2==0)
+        result +=1;
+    }
+
+    return result;
+}
+
+int[] array = CreateArray(lenght);
+PrintArray(array);
+System.Console.WriteLine(FillNumber(array));
---
+++ task36.cs
@@ -0,0 +1,40 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+System.Console.WriteLine("Введите размер массива");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+
+int[]CreateArray (int lenght)
+{
+    int[] array = new int[lenght];
+    for (int i = 0; i<lenght; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    System.Console.WriteLine("["+ string.Join(", ", array)+"]");
+}
+
+int Sum (int[] array)
+{
+    int sum =0;
+    for (int i = 1; i < lenght; i++)
+    {
+        if (i%2!=0)
+        {
+            sum +=array[i];
+        }
+    }
+    return sum;
+}
+
+int[] array = CreateArray(lenght);
+PrintArray(array);
+System.Console.WriteLine(Sum(array));
